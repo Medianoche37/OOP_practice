d363fb1dd2997cd5db00535c8756327848a9a2ae
Nastasya <nastena_rozova@mail.ru>
2025-01-04 22:44:42 +0300

    Первый шаг

--- lesson.py
+++ lesson.py
@@ -1,17 +1,3 @@
-class Numbers:
-    def __init__(self, *args):
-        self.my_list = list(args)
 
-    def add_number(self, n):
-        self.my_list.append(n)
-
-    def get_positive(self):
-        return [i for i in self.my_list if i > 0]
-
-    def get_negative(self):
-        return [i for i in self.my_list if i < 0]
-
-    def get_zeroes(self):
-        return [i for i in self.my_list if i == 0]
 
 
